dd874ca9a6bc3d3ba3127d1448a1b8c9c344b7a5
Your Name <Sachinsharmame40@gmail.com>
2022-03-10 14:55:58 -0800

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CssComponent } from './css/css.component';
+import { HtmlComponent } from './html/html.component';
+import { JavascriptComponent } from './javascript/javascript.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"placeholder",
+    component:PlaceholderComponent
+  },
+  {
+    path:"html",
+    component:HtmlComponent
+  },
+  {
+  path:"css",
+  component:CssComponent
+  },
+  {
+    path:"javascript",
+    component:JavascriptComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { HtmlComponent } from './html/html.component';
+import { CssComponent } from './css/css.component';
+import { JavascriptComponent } from './javascript/javascript.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaceholderComponent,
+    HtmlComponent,
+    CssComponent,
+    JavascriptComponent
   ],
   imports: [
     BrowserModule,
